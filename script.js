507af63add582cf0f3c7c33d68ef8411b8f8da23
biznesmangoat-bit <biznesmangoat@gmail.com>
2026-05-27 16:37:12 +0200

    Add Firebase configuration to script.js

---
+++ script.js
@@ -0,0 +1,9 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyB5usLyIAEnqyVI-_AATSsAiYcIutms43I",
+  authDomain: "dentysta-kalendarz2.firebaseapp.com",
+  projectId: "dentysta-kalendarz2",
+  storageBucket: "dentysta-kalendarz2.firebasestorage.app",
+  messagingSenderId: "1055190429163",
+  appId: "1:1055190429163:web:80bba0484975ea43d07dae",
+  measurementId: "G-GLT8DF0BXE"
+};
